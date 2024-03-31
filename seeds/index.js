0db7048a4ccc78984fc0d070ed21b75c0235bd9c
AtHaRaVs <99896281+AtHaRaVs@users.noreply.github.com>
2024-03-31 11:12:53 +0530

    unsplash api, with bootstrap styling and navbar footer partials

--- seeds/index.js
+++ seeds/index.js
@@ -20,9 +20,14 @@ const seedDb = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city},${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/11649432",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, maiores enim,omnis ipsum exercitationem laudantium possimus quod distinctio atque, repellat perspiciatis veritatis nulla totam eius odio. Praesentium quasi recusandae corporis!",
+      price,
     });
     await camp.save();
   }
